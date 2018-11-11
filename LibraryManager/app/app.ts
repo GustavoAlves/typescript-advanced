1e733f6e9d08ea76593cd51d5fd3b7780f619186
GustavoAlves <gustavo_alves@outlook.com>
2018-11-11 01:22:33 -0200

    Destructuring arrays and objects

--- LibraryManager/app/app.ts
+++ LibraryManager/app/app.ts
@@ -4,4 +4,46 @@ import { Author, Book, Librarian, Logger, Magazine } from "./../models/interface
 import * as util from "./../lib/utilityFunctions";
 import { ReferenceItem } from "./../models/classes/ReferenceItem";
 
-console.log("Hello Advanced TypeScript course on Pluralsight");
+// console.log("Hello Advanced TypeScript course on Pluralsight");
+
+/**
+ * Going Further with Bacic Types
+ */
+
+// Destructuring arrays
+const medals: Array<string> = ["gold", "silver", "bronze"];
+const [first, second, third] = medals;
+
+// Destructuring objects
+const person = {
+	address: "123 Main Street",
+	name: "Audrey",
+	phone: "555-1212"
+};
+const { address, name, phone } = person;
+
+// function printBookInfo(item: Book): void {
+// 	console.log(`${item.title} was authored by ${item.author}`);
+// }
+
+function printBookInfo({ title: booktitle, author: bookauthor }: Book): void {
+	console.log(`${booktitle} was authored by ${bookauthor}`);
+}
+
+// function logFavoriteBooks([book1, book2]: Array<Book>): void {
+// 	printBookInfo(book1);
+// 	printBookInfo(book2);
+// }
+
+function logFavoriteBooks([...books]: Array<Book>): void {
+	books.forEach(book => printBookInfo(book));
+}
+
+// logFavoriteBooks(util.GetAllBooks());
+
+const [book1, book2] = util.GetAllBooks();
+const { title: booktitle, author: bookauthor } = book1;
+console.log(booktitle);
+console.log(bookauthor);
+
+// printBookInfo(book1);
--- LibraryManager/js/app/app.js
+++ LibraryManager/js/app/app.js
@@ -1,4 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello Advanced TypeScript course on Pluralsight");
+var util = require("./../lib/utilityFunctions");
+// console.log("Hello Advanced TypeScript course on Pluralsight");
+/**
+ * Going Further with Bacic Types
+ */
+// Destructuring arrays
+var medals = ["gold", "silver", "bronze"];
+var first = medals[0], second = medals[1], third = medals[2];
+// Destructuring objects
+var person = {
+    address: "123 Main Street",
+    name: "Audrey",
+    phone: "555-1212"
+};
+var address = person.address, name = person.name, phone = person.phone;
+// function printBookInfo(item: Book): void {
+// 	console.log(`${item.title} was authored by ${item.author}`);
+// }
+function printBookInfo(_a) {
+    var booktitle = _a.title, bookauthor = _a.author;
+    console.log(booktitle + " was authored by " + bookauthor);
+}
+// function logFavoriteBooks([book1, book2]: Array<Book>): void {
+// 	printBookInfo(book1);
+// 	printBookInfo(book2);
+// }
+function logFavoriteBooks(_a) {
+    var books = _a.slice(0);
+    books.forEach(function (book) { return printBookInfo(book); });
+}
+// logFavoriteBooks(util.GetAllBooks());
+var _a = util.GetAllBooks(), book1 = _a[0], book2 = _a[1];
+var booktitle = book1.title, bookauthor = book1.author;
+console.log(booktitle);
+console.log(bookauthor);
+// printBookInfo(book1);
 //# sourceMappingURL=app.js.map
