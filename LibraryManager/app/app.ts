6e4263fd1dd7ada6d65b0dd1a0e7afd2c4340bbf
GustavoAlves <gustavo_alves@outlook.com>
2018-11-12 00:12:46 -0200

    Promises

--- LibraryManager/app/app.ts
+++ LibraryManager/app/app.ts
@@ -15,32 +15,80 @@ import { Author, Book, KeyValuePair, Librarian, Logger, Magazine } from "./../mo
  * Implementing Asynchronous Patterns
  */
 // Callback Functions
-type LibMgrCallback = (err: Error, titles: Array<string>) => void;
+// type LibMgrCallback = (err: Error, titles: Array<string>) => void;
 
-function getBooksByCategory(cat: Category, callback: LibMgrCallback): void {
-	setTimeout(() => {
-		try {
+// function getBooksByCategory(cat: Category, callback: LibMgrCallback): void {
+// 	setTimeout(() => {
+// 		try {
+// 			const foundBooks: Array<string> = util.GetBookTitlesByCategory(cat);
+
+// 			if (foundBooks.length > 0) {
+// 				callback(null, foundBooks);
+// 			} else {
+// 				throw new Error("No books found.");
+// 			}
+// 		} catch (err) {
+// 			callback(err, null);
+// 		}
+// 	}, 2000);
+// }
+
+// function logCategorySearch(err: Error, titles: Array<string>): void {
+// 	if (err) {
+// 		console.log(`Error message: ${err.message}`);
+// 	} else {
+// 		console.log(`Found the following titles: ${titles}`);
+// 	}
+// }
+
+// console.log("Beginnig search...");
+// getBooksByCategory(Category.Biography, logCategorySearch);
+// console.log("Search submitted...");
+
+// Promise
+function getBooksByCategory(cat: Category): Promise<Array<string>> {
+	let p: Promise<Array<string>> = new Promise((resolve, reject) => {
+		setTimeout(() => {
 			const foundBooks: Array<string> = util.GetBookTitlesByCategory(cat);
 
 			if (foundBooks.length > 0) {
-				callback(null, foundBooks);
+				resolve(foundBooks);
 			} else {
-				throw new Error("No books found.");
+				reject("No books found for that category.");
 			}
-		} catch (err) {
-			callback(err, null);
-		}
-	}, 2000);
+		}, 2000);
+	});
+
+	return p;
 }
 
-function logCategorySearch(err: Error, titles: Array<string>): void {
-	if (err) {
-		console.log(`Error message: ${err.message}`);
-	} else {
-		console.log(`Found the following titles: ${titles}`);
-	}
-}
+// console.log("Beginnig search...");
+// getBooksByCategory(Category.Fiction)
+// 	.then(titles => {
+// 		console.log(`Found titles: ${titles}`);
+// 		return titles.length;
+// 	})
+// 	.then(numOfBooks => console.log(`Number of books found: ${numOfBooks}`))
+// 	.catch(reason => console.log(`Error: ${reason}`));
+// console.log("Search submitted...");
+
+// console.log("Beginnig search...");
+// getBooksByCategory(Category.Biography)
+// 	.then(titles => {
+// 		console.log(`Found titles: ${titles}`);
+// 		return titles.length;
+// 	}, reason => 0 ) // reason => { return 0; }
+// 	.then(numOfBooks => console.log(`Number of books found: ${numOfBooks}`))
+// 	.catch(reason => console.log(`Error: ${reason}`));
+// console.log("Search submitted...");
 
 console.log("Beginnig search...");
-getBooksByCategory(Category.Biography, logCategorySearch);
+getBooksByCategory(Category.Fiction)
+	.then(titles => {
+		console.log(`Found titles: ${titles}`);
+		throw "Something bad happened";
+		return titles.length;
+	}, reason => 0 ) // reason => { return 0; }
+	.then(numOfBooks => console.log(`Number of books found: ${numOfBooks}`))
+	.catch(reason => console.log(`Error: ${reason}`));
 console.log("Search submitted...");
